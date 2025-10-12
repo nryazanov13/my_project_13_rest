647c721780163b6eb3324f5b43d889eb547d0cd0
RyazanovNY <riazanovnikita@gmail.com>
2025-10-12 10:23:03 +0300

    Added formatting

--- src/test/java/models/UserRequestModel.java
+++ src/test/java/models/UserRequestModel.java
@@ -1,4 +1,5 @@
 package models;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/test/java/models/UserResponseModel.java
+++ src/test/java/models/UserResponseModel.java
@@ -1,6 +1,8 @@
 package models;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserResponseModel {
